74ddb62451c72f4babe541fe8086f00dac59a3d0
Trond Skår <trond.skar@amedia.no>
2016-06-04 22:33:12 +0200

    Added gulpif and minified production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat')
+    gulpif = require('gulp-if');
+    uglify = require('gulp-uglify');
 
 var env,
     coffeeSources,
@@ -47,6 +49,7 @@ gulp.task('js', function(){
   gulp.src(jsSources)
       .pipe(concat('script.js'))
       .pipe(browserify())
+      .pipe(gulpif(env === 'production', uglify()))
       .pipe(gulp.dest(outputDir + 'js'))
       .pipe(connect.reload())
 });
